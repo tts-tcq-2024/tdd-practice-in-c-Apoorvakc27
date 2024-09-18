412cdd15af8bfa29cebd2950b7703880128d68d8
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-18 17:35:02 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,8 +1,18 @@
 #include<stdio.h>
 #include<string.h>
 
+
+int validate_string(const char *str)
+{
+    return 0;
+}
+
 int Addition_NumUsingString(const char *str)
 {
+    if(validate_string(str))
+    {
+        return 0;
+    }
     return -1;
 }
 
