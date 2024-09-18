f4f63e8590c273d0f289fa148c42dc86d9e3cec9
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-18 18:01:41 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,46 @@
 #include<stdio.h>
 #include<string.h>
 
+int neg_loop(const char *str,int i)
+{
+      if(str[i] == '-')
+      {
+         return 0;
+      }
+    return 1;
+}
+
+int  check_negative_char(const char *str)
+{
+     int result = 0,i=0;
+    while(ch[i] != '\0')
+    {
+      result = neg_loop(str,i);
+      if(result == 0)
+      {
+          break;
+      }
+      i++;
+    }
+    return result;
+}
+
+int  check_empty_str(const char *str)
+{
+    int strlength = strlen(str);
+
+    if(strlength == 0)
+    {
+        return 1;
+    }
+    return 0;
+}    
 
 int validate_string(const char *str)
 {
     if(str != NULL)
     {
-        return 1;
+        return check_negative_char(str) || check_empty_str(str);
     }
     return 0;
 }
